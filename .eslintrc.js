c32966519e2246953c5b22ade38e047ff4739e36
Lucas <lucascruzati@gmail.com>
2021-10-06 22:40:31 -0300

    feat: added initial files

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: [
+    'standard-with-typescript'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off'
+  }
+}
---
+++ src/main/server.ts
@@ -0,0 +1,6 @@
+import env from './config/env';
+
+(async () => {
+  const app = (await import('./config/app')).default;
+  app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`));
+})();
