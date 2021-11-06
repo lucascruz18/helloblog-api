c91fd7a4e079a3e9b19f52a6e2cc0bdde47036c5
Lucas <lucascruzati@gmail.com>
2021-11-06 20:20:43 -0300

    test: add compare field validation

---
+++ tests/validation/validators/compare-field-validation.spec.ts
@@ -0,0 +1,26 @@
+import { CompareFieldsValidation } from './compare-fields-validation'
+import { InvalidParamError } from '../../presentation/errors'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+
+describe('Required field validation', () => {
+  test('Should return a InvalidParamError if validations fails ', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_field',
+      fieldToCompare: 'wrong_field'
+    })
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  test('Should not return if validations succeds ', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      field: 'any_field',
+      fieldToCompare: 'any_field'
+    })
+    expect(error).toBeFalsy()
+  })
+})
